fcae1847890cba6f37eb01ef3b80918911e48ab2
Musarrat Jahan <jahan786musarrat.786@gmail.com>
2026-07-01 15:52:56 +0530

    code optimised

--- 02_Arrays_and_Hashing/L41_first_missing_positive.py
+++ 02_Arrays_and_Hashing/L41_first_missing_positive.py
@@ -13,25 +13,44 @@ class Solution:
         #         return i
         # return maxi+1
 
-        n=len(nums)
-        print(n)
-        for i in range(n):
-            if nums[i]<0:
-                nums[i]=0
+        # n=len(nums)
+        # print(n)
+        # for i in range(n):
+        #     if nums[i]<0:
+        #         nums[i]=0
 
-        for num in nums:
-            val=abs(num)
-            if 1<=val<=n:
-                if nums[val-1]>0:
-                    nums[val-1]*=-1
-                elif nums[val-1]==0:
-                    nums[val-1]=-1*(n+1)
+        # for num in nums:
+        #     val=abs(num)
+        #     if 1<=val<=n:
+        #         if nums[val-1]>0:
+        #             nums[val-1]*=-1
+        #         elif nums[val-1]==0:
+        #             nums[val-1]=-1*(n+1)
         
+        # for i in range(n):
+        #     if nums[i]>0:
+        #         return i+1
+        # return n+1
+
+        #using cycle sort
+        n=len(nums)
+        i=0
+        while i<n:
+            if nums[i]<=0 or nums[i]>n:
+                i+=1
+                continue
+            index=nums[i]-1
+            if nums[i]!=nums[index]:
+                nums[i],nums[index]=nums[index],nums[i]
+            else:
+                i+=1
+
         for i in range(n):
-            if nums[i]>0:
+            if nums[i]!=i+1:
                 return i+1
         return n+1
-
+    
+    
 if __name__=='__main__':
     sol = Solution()
     nums = [98,93,95,10,91,4,90,88,56,84,65,62,83,80,78,60,73,77,76,29,63,12,57,17,69,68,50,11,31,33,8,42,38,7,0,37,48,26,20,44,46,43,52,51,47,18,49,58,2,39,30,81,22,55,36,40,15,27,21,32,64,41,53,19,28,24,9,25,3,59,66,82,61,70,23,34,71,54,74,-1,1,45,14,79,5,35,13,72,75,85,87,6,16,86,67,89,94,92,96,97]
